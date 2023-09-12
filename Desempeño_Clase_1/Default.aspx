4de4ff3b7ce8d046b881123485a184252a443619
BrunoBonamino <132159071+BrunoBonamino@users.noreply.github.com>
2023-09-12 19:50:29 -0300

    Clase 1

---
+++ Desempeño_Clase_1/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 281px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Ingresa un Número:<asp:TextBox ID="Numero" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Selecciona que desea Saber:<br />
+        <asp:CheckBox ID="CB_Centenas" runat="server" OnCheckedChanged="CB_Centenas_CheckedChanged" Text="Centenas" />
+    
+    </div>
+        <p>
+            <asp:CheckBox ID="CB_Decenas" runat="server" OnCheckedChanged="CB_Decenas_CheckedChanged" Text="Decenas" />
+        </p>
+        <p>
+            <asp:CheckBox ID="CB_Unidades" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="Unidades" />
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calcular" />
+        <p>
+            <asp:Label ID="Resultado" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Desempeño_Clase_1/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    
+    protected void CB_Centenas_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CB_Decenas_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Numero.Text != "") 
+        {
+            var numero = 0;
+            var centena = 0;
+            var decena = 0;
+            var unidad = 0;
+            var resultado = "";
+            if (CB_Centenas.Checked)
+            {
+                 numero = int.Parse(this.Numero.Text);
+                 centena = numero / 100;
+                 resultado = "Centenas:" + (centena) + " ";
+            }
+             if (CB_Decenas.Checked)
+            {
+                 numero = int.Parse(this.Numero.Text);
+                 centena = numero / 100;
+                 decena = (numero - (centena * 100)) / 10;
+                 resultado = resultado + "Decenas:" + (decena) + " ";
+            }
+             if (CB_Unidades.Checked)
+            {
+                numero = int.Parse(this.Numero.Text);
+                centena = numero / 100;
+                decena = (numero - (centena * 100)) / 10;
+                unidad = (numero - (centena * 100 + decena * 10));
+                resultado = resultado + "Unidades:" + (unidad);
+            }
+             Resultado.Text = resultado;
+        }
+        else
+        {
+            Resultado.Text = "Debe ingresar un número";
+        }
+    }
+
+    // override object.GetHashCode
+    public override int GetHashCode()
+    {
+        // TODO: write your implementation of GetHashCode() here
+        throw new NotImplementedException();
+        return base.GetHashCode();
+    }
+    }
